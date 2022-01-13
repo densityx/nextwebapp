bcedcfa134f7706002abe37056be7d0f84d32df2
Ahmad Aziz <ahmadaziz0441@gmail.com>
2022-01-13 11:46:46 +0800

    specify anonymous as the value of the Google font CDN crossorigin

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
                 <meta name="description" content="Next Webapp by Ahmad Aziz" />
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
             </Head>
 
